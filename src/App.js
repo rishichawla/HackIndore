e1e471cba9a19e22919e33c9fae43354cb279d95
Rishi Chawla <rishi.chawla14@gmail.com>
2018-09-28 20:41:41 +0530

    Added input fields

--- src/App.js
+++ src/App.js
@@ -3,16 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Your Intelligent Keyboard</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="form">
+          <form onSubmit={this.handleSubmit}>
+            <p> Enter text to be translated: </p>
+            <textarea value={this.state.value} id="itext" onSubmit={this.state.value} onChange={this.handleChange} />
+            <button id="translate"> Translate </button>
+            <p> Translated text: </p>
+            <textarea value={this.state.value} id="otext" onChange={this.handleChange} />
+          </form>
+        </div>
       </div>
     );
   }
