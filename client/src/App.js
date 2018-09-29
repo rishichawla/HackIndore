8113e87e4753e79fbcb7e25224f70c32fb266b41
Rishi Chawla <rishi.chawla14@gmail.com>
2018-09-29 14:33:03 +0530

    Integrated API

--- client/src/App.js
+++ client/src/App.js
@@ -7,72 +7,50 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = { value: 'hey node' };
-
-
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
- 
+
   handleChange(event) {
-    this.setState({ value: event.target.value });
+    this.setState({ name: event.target.value });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-  }
-  componentDidMount(){
-
     const user = {
-      value: this.state.value
+      name: this.state.name
     };
-
-
-    Axios.post('http://localhost:5000/hello', {user})
-    .then(res => {
-      console.log(res);
-     
-      // if (res)
-      // {
-      // const d=res.detectedLanguage.language;
-      // this.setState({d});
-      // }
-      // else {
-      //   this.setState="None";
-      // }
-    })
   }
-  /*componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }*/
 
-  /*callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-    if (response.status !== 200)
-      throw Error(body.message);
-    return body;
-  };*/
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Your Intelligent Keyboard</h1>
-        </header>
-        <div className="form">
-          <form onSubmit={this.handleSubmit}>
-            <p> Enter text to be translated: </p>
-            <textarea id="itext" onChange={this.handleChange} />
-            <button id="translate"> Translate </button>
-            <p> Translated text: </p>
-            <textarea value={this.state} id="otext" onChange={this.handleChange} />
-          </form>
-        </div>
+  const user = {
+    name: this.state.name
+  };
+
+  Axios.post('http://localhost:5000/hello', { user })
+      .then(res => {
+    res.send("data got");
+console.log(res);
+      })
+
+render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Your Intelligent Keyboard</h1>
+      </header>
+      <div className="form">
+        <form onSubmit={this.handleSubmit}>
+          <p> Enter text to be translated: </p>
+          <textarea id="itext" name="text" onChange={this.handleChange} />
+          <button id="translate" type="submit"> Translate </button>
+          <p> Translated text: </p>
+          <textarea value={this.state} id="otext" onChange={this.handleChange} />
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
+}
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,104 +1,56 @@
 'use strict';
 const express = require('express');
-
 const app = express();
-const port = process.env.PORT || 5000;
 var cors = require('cors');
-app.use(cors()); 
-
-var bodyParser = require('body-parser')
+app.use(cors());
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+const axios = require('axios');
+const { Translate } = require('@google-cloud/translate');
+const projectId = 'smartkey-217906';
+const translate = new Translate({
+    projectId: projectId,
+});
+const target = 'hi';
 
-const axios = require('axios')
+const d = "";
 
-// var corsOptions = {
-//     origin: 'http://localhost:3000',
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-//   }
+/*app.post('/hello', (req, res) => {
+    d = req.body;
+    console.log(d);
+})*/
 
-
-
-// let fs = require('fs');
-// let https = require('https');
-// let url = require('url');
-
-// let subscriptionKey = '9d98aeeda45d4249b9bc791f39458110';
-// let host = 'api.cognitive.microsofttranslator.com';
-// let path = '/translate?api-version=3.0';
-// let params = '&to=hi';
-
-// let text = 'Hello, world!';
-
-app.get('/api' , (req , res)=> {
+app.get('/api', (req, res) => {
     res.send('hey react');
     res.end();
 })
 
-app.post('/hello', (req, res)=> {
-    console.log(req.body);
-    res.send('data got')
-})
+const text = 'Hello, world!';
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+translate
+    .translate(text, target)
+    .then(results => {
+        const translation = results[0];
 
+        console.log(`Text: ${text}`);
+        console.log(`Translation: ${translation}`);
+    })
+    .catch(err => {
+        console.error('ERROR:', err);
+    });
 
+translate
+    .detect(text)
+    .then(results => {
+        let detections = results[0];
+        detections = Array.isArray(detections) ? detections : [detections];
 
-
-// app.get('/api/hello', (req, res) => {
-//     console.log('hello theere');
-    
-//     let response_handler = function (response) {
-//         let body = '';
-//         console.log(response);
-//         response.on('data', function (d) {
-//             body += d;
-//         });
-//         response.on('end', function () {
-//             console.log(response.url);
-//             let json = JSON.stringify(JSON.parse(body), null, 4);
-//             console.log(json);
-//         });
-//         response.on('error', function (e) {
-//             console.log('Error: ' + e.message);
-//         });
-//     };
-
-//     let get_guid = function () {
-//         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-//             var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-//             return v.toString(16);
-//         });
-//     }
-
-//     let Translate = function (content) {
-//         let request_params = {
-//             method: 'POST',
-//             hostname: host,
-//             path: path + params,
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'Ocp-Apim-Subscription-Key': subscriptionKey,
-//                 'X-ClientTraceId': get_guid(),
-//             }
-//         };
-
-//         let req = https.request(request_params, response_handler);
-//         req.write(content);
-//         /*console.log(url.parse(req.url).pathname);*/
-//         //console.log(req);
-//         req.end();
-//     }
-//     let content = JSON.stringify([{ 'Text': text }]);
-//     //res.send(content);
-
-//     // axios.post('http://localhost:5000/api/hello', {
-//     //     content
-//     // })
-//     // .then(response => { 
-//     //     console.log(response)
-//     // })
-//     // .catch(error => {
-//     //     console.log(error.response)
-//     // });
-// });
+        console.log('Detections:');
+        detections.forEach(detection => {
+            console.log(`${detection.input} => ${detection.language}`);
+        });
+    })
+    .catch(err => {
+        console.error('ERROR:', err);
+    });
